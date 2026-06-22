744aee4c28487c92340acae453cf4707f25cc92f
kimocks-netizen <brynetinashechikomo@gmail.com>
2026-06-22 22:44:43 +0200

    FIXED: CORS Headers

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -1,15 +1,33 @@
 const BACKEND_URL = "http://13.247.70.10:8000";
 
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export const onRequest = async (context: any) => {
+  if (context.request.method === "OPTIONS") {
+    return new Response(null, { status: 204, headers: CORS_HEADERS });
+  }
+
   const url = new URL(context.request.url);
   const backendUrl = new URL(url.pathname + url.search, BACKEND_URL);
 
-  return fetch(backendUrl.toString(), {
+  const res = await fetch(backendUrl.toString(), {
     method: context.request.method,
     headers: context.request.headers,
     body:
       context.request.method === "GET" || context.request.method === "HEAD"
         ? undefined
         : context.request.body,
+  });
+
+  const newHeaders = new Headers(res.headers);
+  Object.entries(CORS_HEADERS).forEach(([k, v]) => newHeaders.set(k, v));
+
+  return new Response(res.body, {
+    status: res.status,
+    headers: newHeaders,
   });
 };
